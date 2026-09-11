185bdeca994fd781b3f1e31b95c0a60281ef2128
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-11 18:31:05 +0300

    feat: add create cart func

--- internal/service/cart.go
+++ internal/service/cart.go
@@ -15,12 +15,27 @@ func (s *CartService) CreateCart(ctx context.Context) (entity.CreateCartResponse
 	return entity.CreateCartResponse{ID: cart.ID, Items: make([]entity.CartItem, 0)}, nil
 }
 
-func ViewCart() {}
+func (s *CartService) ViewCart() {
+	//TODO implement me
+	panic("implement me")
+}
 
-func AddItem() {}
+func (s *CartService) AddItem() {
+	//TODO implement me
+	panic("implement me")
+}
 
-func UpdateItem() {}
+func (s *CartService) UpdateItem() {
+	//TODO implement me
+	panic("implement me")
+}
 
-func RemoveItem() {}
+func (s *CartService) RemoveItem() {
+	//TODO implement me
+	panic("implement me")
+}
 
-func CalculatePrice() {}
+func (s *CartService) CalculatePrice() {
+	//TODO implement me
+	panic("implement me")
+}
