cd0cef1fe90847c2d0f80917dec9f8f08f87cd12
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-11 18:34:15 +0300

    fix: fix add cart func

--- internal/repository/operations.go
+++ internal/repository/operations.go
@@ -7,15 +7,16 @@ import (
 	"github.com/maxon2034/trainee-go-cart-api/internal/entity"
 )
 
-func (r *CartRepository) AddCart(ctx context.Context, cart *entity.Cart) (*entity.Cart, error) {
+func (r *CartRepository) AddCart(ctx context.Context) (*entity.Cart, error) {
+	var cart entity.Cart
 	query := `
 		INSERT INTO carts DEFAULT VALUES
 		RETURNING id, created_at;
 	`
-	if err := r.DB.QueryRowContext(ctx, query).Scan(&cart.ID, cart.CreatedAt); err != nil {
+	if err := r.DB.QueryRowContext(ctx, query).Scan(&cart.ID, &cart.CreatedAt); err != nil {
 		return nil, fmt.Errorf("error in creating cart: %w", err)
 	}
-	return cart, nil
+	return &cart, nil
 }
 
 func (r *CartRepository) GetCart(ctx context.Context, id string) (*entity.Cart, error)    {}
