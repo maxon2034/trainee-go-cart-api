7e016e5f3f2f6de912c49d69fb229f2363330e76
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-11 18:18:46 +0300

    feat: add custom server

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -1,1 +1,25 @@
 package handlers
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"github.com/maxon2034/trainee-go-cart-api/internal/config"
+)
+
+func NewServer(ctx context.Context, cfg config.Config, service Service) *http.Server {
+	mux := http.NewServeMux()
+
+	cartHandler := NewCartHandler(service)
+
+	mux.HandleFunc("POST /api/v1/carts", cartHandler.Create)
+
+	return &http.Server{
+		Addr:    cfg.Server.Port,
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			return ctx
+		},
+	}
+}
