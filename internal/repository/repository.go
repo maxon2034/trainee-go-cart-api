b570f28d0f919e9dc8a4352bd64167f63f42bda9
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-11 11:16:45 +0300

    feat: add repositoy

---
+++ internal/repository/repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+type CartRepository struct {
+	db sqlx.DB
+}
+
+func NewRepository(db sqlx.DB) *CartRepository {
+	return &CartRepository{db: db}
+}
