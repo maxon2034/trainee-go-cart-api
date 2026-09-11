ddf3c04ee534d5b4db629a4e9eefd1736797620d
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-11 18:18:46 +0300

    feat: add custom server

--- internal/handlers/create_cart.go
+++ internal/handlers/create_cart.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -10,16 +11,16 @@ type CartHandler struct {
 	service Service
 }
 
-func NewCartHandler(service Service) *CartHandler {
-	return &CartHandler{service: service}
+func NewCartHandler(service Service) CartHandler {
+	return CartHandler{service: service}
 }
 
-func (h *CartHandler) CreateCartHandler(w http.ResponseWriter, r *http.Request) {
+func (h *CartHandler) Create(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	cartResp, err := h.service.CreateCart()
+	cartResp, err := h.service.CreateCart(context.Background())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
--- internal/handlers/service.go
+++ internal/handlers/service.go
@@ -1,9 +1,13 @@
 package handlers
 
-import "github.com/maxon2034/trainee-go-cart-api/internal/entity"
+import (
+	"context"
+
+	"github.com/maxon2034/trainee-go-cart-api/internal/entity"
+)
 
 type Service interface {
-	CreateCart() (entity.CreateCartResponse, error)
+	CreateCart(ctx context.Context) (entity.CreateCartResponse, error)
 	ViewCart()
 	AddItem()
 	UpdateItem()
