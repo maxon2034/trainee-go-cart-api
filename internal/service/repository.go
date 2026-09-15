cdefbac1ef1cc5f48b76d97b85146a149394207a
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-15 15:51:16 +0300

    fix: fix entity naming in GetCart

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -9,7 +9,7 @@ import (
 type Repository interface {
 	AddCart(ctx context.Context) (*entity.Cart, error)
 	// TODO: Rename Cart DTO
-	GetCart(ctx context.Context, id int) (*entity.CreateCartResponse, error)
+	GetCart(ctx context.Context, id int) (*entity.CartDTO, error)
 	AddCartItem(ctx context.Context, item *entity.CartItem) error
 	UpdateCartItem(ctx context.Context, item *entity.CartItem) error
 	RemoveCartItem(ctx context.Context, cartID, itemID string) error
