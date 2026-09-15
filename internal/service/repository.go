87b83a3758c8a5ca1e8aafade13d76c8848873c8
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-15 15:08:27 +0300

    feat: add ViewCart service method

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -8,7 +8,8 @@ import (
 
 type Repository interface {
 	AddCart(ctx context.Context) (*entity.Cart, error)
-	GetCart(ctx context.Context, id string) (*entity.Cart, error)
+	// TODO: Rename Cart DTO
+	GetCart(ctx context.Context, id int) (*entity.CreateCartResponse, error)
 	AddCartItem(ctx context.Context, item *entity.CartItem) error
 	UpdateCartItem(ctx context.Context, item *entity.CartItem) error
 	RemoveCartItem(ctx context.Context, cartID, itemID string) error
