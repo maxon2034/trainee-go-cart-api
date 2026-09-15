9c4a0d47b16437c6c9a9328e59a64117bb17858c
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-15 12:37:14 +0300

    feat: add logger into main

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
-	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/joho/godotenv"
+	"github.com/maxon2034/trainee-go-cart-api/internal/app"
 	"github.com/maxon2034/trainee-go-cart-api/internal/config"
 	"github.com/maxon2034/trainee-go-cart-api/internal/db"
 )
@@ -13,31 +14,33 @@ import (
 var cfgPath string = "config/"
 
 func main() {
-	// logger := app.Logger
+	logger := app.NewLogger()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	if err := godotenv.Load(".env"); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		logger.Error("Error loading .env file", slog.Any("Error", err))
+		return
 	}
 
 	cfg, err := config.Load(cfgPath)
 	if err != nil {
-		log.Print(err)
+		logger.Error("Error loading config", slog.Any("Error", err))
 	}
 
 	DB, err := db.NewPostgres(ctx, cfg)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Error initializing DB", slog.Any("Error", err))
 	}
+
 	defer func() {
 		if err := DB.Close(); err != nil {
-			log.Fatal(err)
+			logger.Error("Error closing DB", slog.Any("Error", err))
 		}
 	}()
 
 	if err := db.RunMigrations(DB); err != nil {
-		log.Fatal(err)
+		logger.Error("Error running migrations", slog.Any("Error", err))
 	}
 
 	mux := http.NewServeMux()
@@ -45,6 +48,6 @@ func main() {
 	// handlers
 
 	if err = http.ListenAndServe(cfg.Server.Port, mux); err != nil {
-		log.Fatal("error in starting server: ", err)
+		logger.Error("Error starting server", slog.Any("Error", err))
 	}
 }
