f69d609e50c88eb1327d69ce392aa83e03e938b0
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-09-15 12:30:44 +0300

    feat: add logger init

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,14 @@
 package app
 
+import (
+	"log/slog"
+	"os"
+)
+
 // TODO: Logger init
+func NewLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(os.Stdout, nil))
+}
 
 // TODO: Server struct init
 
